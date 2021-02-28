8e3546a6a6558ebfdbd7140124176ce5300a530d
ummeyoni <ummeyoni1995@gmail.com>
2021-02-27 21:26:07 -0800

    added again

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
+import React, { createContext, useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home/Home";
 import Appointment from "./components/Appointment/Appointment/Appointment";
+import Login from "./components/Login/Login/Login";
+import Dashboard from "./components/Dashboard/Dashboard/Dashboard";
+import AllPatients from "./components/AllPatients/AllPatients/AllPatients";
+import AddDoctor from "./components/AddDoctor/AddDoctor";
 
+export const UserContext = createContext();
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home> </Home>
-        </Route>
-        <Route exact path="/appointment">
-          <Appointment></Appointment>
-        </Route>
-      </Switch>
-    </Router>
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home> </Home>
+          </Route>
+          <Route exact path="/appointment">
+            <Appointment></Appointment>
+          </Route>
+          <Route exact path="/dashboard/appointment">
+            <Dashboard></Dashboard>
+          </Route>
+          <Route path="/dashboard/allPatients">
+            <AllPatients></AllPatients>
+          </Route>
+          <Route path="/addDoctor">
+            <AddDoctor></AddDoctor>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/Login/Login/firebase.config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAz1CJro7XHmGKj6No27E1-M2YpGJDnF5Q",
+  authDomain: "doctors-portal-fbbb5.firebaseapp.com",
+  projectId: "doctors-portal-fbbb5",
+  storageBucket: "doctors-portal-fbbb5.appspot.com",
+  messagingSenderId: "775793292708",
+  appId: "1:775793292708:web:7baa961c15bdda26ecfd31"
+};
+export default firebaseConfig;
