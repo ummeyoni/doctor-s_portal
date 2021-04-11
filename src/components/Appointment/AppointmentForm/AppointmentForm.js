96727898833c265834aef769479274d842ec155e
ummeyoni <ummeyoni1995@gmail.com>
2021-04-11 09:22:47 +0600

    committed again

--- src/components/Appointment/AppointmentForm/AppointmentForm.js
+++ src/components/Appointment/AppointmentForm/AppointmentForm.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
 import { useForm } from "react-hook-form";
-
+const bookingData = [
+  {
+    _id: "5e8df50be6e8231764dc23de",
+    id: 1,
+    subject: "Teeth Orthodontics",
+    visitingHour: "8:00 AM - 9:00 AM",
+    totalSpace: "10 SPACES AVAILABLE"
+  },
+  {
+    _id: "5e8df578e6e8231764dc23df",
+    id: 2,
+    subject: "Cosmetic Dentistry",
+    visitingHour: "10:50 AM - 11:30 AM",
+    totalSpace: "10 SPACES AVAILABLE"
+  },
+  {
+    _id: "5e8df5aee6e8231764dc23e0",
+    id: 3,
+    subject: "Teeth Cleaning",
+    visitingHour: "5:00 PM - 6:00 PM",
+    totalSpace: "10 SPACES AVAILABLE"
+  },
+  {
+    _id: "5e8df63be6e8231764dc23e1",
+    id: 4,
+    subject: "Cavity Protection",
+    visitingHour: "7:00 AM - 8:30 AM",
+    totalSpace: "10 SPACES AVAILABLE"
+  },
+  {
+    _id: "5e8df68de6e8231764dc23e2",
+    id: 5,
+    subject: "Teeth Orthodontics",
+    visitingHour: "8:00 AM - 9:00 AM",
+    totalSpace: "10 SPACES AVAILABLE"
+  },
+  {
+    _id: "5e8df6a0e6e8231764dc23e3",
+    id: 6,
+    subject: "Teeth Orthodontics",
+    visitingHour: "8:00 AM - 9:00 AM",
+    totalSpace: "10 SPACES AVAILABLE"
+  }
+];
 const customStyles = {
   content: {
     top: "50%",
@@ -15,24 +58,41 @@ const customStyles = {
 Modal.setAppElement("#root");
 
 const AppointmentForm = ({ modalIsOpen, closeModal, appointmentOn, date }) => {
+  // my implement code
+  const [check, setCheck] = useState([]);
+  console.log(check.length);
+  useEffect(() => {
+    fetch(
+      "https://stark-temple-71384.herokuapp.com/appointments?service=" +
+        appointmentOn
+    )
+      .then(res => res.json())
+      .then(data => setCheck(data));
+  }, []);
+
   const { register, handleSubmit, errors } = useForm();
   const onSubmit = data => {
     data.service = appointmentOn;
     data.date = date;
     data.created = new Date();
-
-    fetch("https://stark-temple-71384.herokuapp.com/addAppointment", {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(data)
-    })
-      .then(res => res.json())
-      .then(success => {
-        if (success) {
-          closeModal();
-          alert("Appointment created successfully.");
-        }
-      });
+    if (check.length < 10) {
+      fetch("https://stark-temple-71384.herokuapp.com/addAppointment", {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(success => {
+          if (success) {
+            closeModal();
+            alert("Appointment created successfully.");
+            window.location.reload();
+          }
+        });
+    } else {
+      alert("Sorry space not avalable ");
+      closeModal();
+    }
   };
   return (
     <div>
--- src/components/Appointment/BookingCard/BookingCard.js
+++ src/components/Appointment/BookingCard/BookingCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppointmentForm from "../AppointmentForm/AppointmentForm";
 
 const BookingCard = ({ booking, date }) => {
@@ -10,21 +10,62 @@ const BookingCard = ({ booking, date }) => {
   function closeModal() {
     setIsOpen(false);
   }
+
+  // my implement code
+
+  const [check, setCheck] = useState([]);
+  console.log(check.length);
+  useEffect(() => {
+    fetch("https://stark-temple-71384.herokuapp.com/appointments?service=" + booking.subject)
+      .then(res => res.json())
+      .then(data => setCheck(data));
+  }, []);
+
+  // for availabe sites
+  const AvailableSites = 10 - check.length;
+
+  // click and change btn
+
+  const [isClicked, setIsClicked] = useState(false);
+  // console.log(isClicked);
+  // useEffect(() => {
+  //   const data = check.length === 10;
+  // setIsClicked(data)
+  // },[check.length]);
+
+  const newdate = new Date();
+  const isvalid = date.toDateString() > newdate.toDateString();
+  console.log(isvalid);
+
   return (
     <div className="col-md-4 ">
       <div class="card mb-5">
         <div class="card-body p-3">
           <h3 style={{ color: "#1cc7c1" }}>{booking.subject}</h3>
           <h4>{booking.visitingHour}</h4>
-          <small>{booking.totalSpace}</small>
+          <h6>
+            <small> total space:10</small>
+          </h6>
+          <small>{AvailableSites} SPACES AVALABLE</small>
           <br />
+
           <button
-            onClick={openModal}
-            style={{ backgroundColor: "#1cc7c1" }}
+            disabled={isvalid}
+            onClick={() => {
+              openModal();
+            }}
             className="btn btn-primary"
           >
-            BOOK APPOINTMENT
+            {isvalid ? "× Can't book" : "BOOK APPOINTMENT"}
           </button>
+
+          {/* <button disabled={isClicked} onClick={() => {
+           openModal()
+           
+          }} className="btn btn-primary">
+            {isClicked ? "× Can't book" : "BOOK APPOINTMENT"}
+          </button> */}
+
           <AppointmentForm
             modalIsOpen={modalIsOpen}
             appointmentOn={booking.subject}
